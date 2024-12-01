38ecb9464def4e077a5364401c3dca448000c261
silaiyi <darknesstimer@gmail.com>
2024-12-01 20:12:13 +0800

    新增了一個new empty project

---
+++ MobileAppFinalProject/app/src/main/java/ict/thei/mobileappfinalproject/MainActivity.java
@@ -0,0 +1,14 @@
+package ict.thei.mobileappfinalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
